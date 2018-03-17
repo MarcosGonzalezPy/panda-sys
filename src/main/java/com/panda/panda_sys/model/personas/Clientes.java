df792136688e0fcbc3998ee538856954f310ee9d
Aurora Benitez <ajbcgonzaleza@gmail.com>
2018-03-17 18:15:06 -0400

    se agrego un campo nuevo a la tabla clientes tipo_persona

--- src/main/java/com/panda/panda_sys/model/personas/Clientes.java
+++ src/main/java/com/panda/panda_sys/model/personas/Clientes.java
@@ -1,21 +1,33 @@
 package com.panda.panda_sys.model.personas;
 
-public class Clientes extends Personas{
-	
+public class Clientes extends Personas {
+
 	private String razonSocial;
 	private String limiteCredito;
-	
+	private String tipoPersona;
+
 	public String getRazonSocial() {
 		return razonSocial;
 	}
+
 	public void setRazonSocial(String razonSocial) {
 		this.razonSocial = razonSocial;
 	}
+
 	public String getLimiteCredito() {
 		return limiteCredito;
 	}
+
 	public void setLimiteCredito(String limiteCredito) {
 		this.limiteCredito = limiteCredito;
-	} 
-	
+	}
+
+	public String getTipoPersona() {
+		return tipoPersona;
+	}
+
+	public void setTipoPersona(String tipoPersona) {
+		this.tipoPersona = tipoPersona;
+	}
+
 }
